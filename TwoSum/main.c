d90167e69b219695a94f3684b9cbebd2b3fbdb29
One Graund <vovangaunder@gmail.com>
2025-07-27 23:31:30 +0200

    Final TwoSum bruteforce solution

--- TwoSum/main.c
+++ TwoSum/main.c
@@ -5,7 +5,7 @@ int *twoSum(int *nums, int numssize, int target, int *returnsize) {
 	int *indices = malloc(sizeof(int) * 2);
 	for (int i = 0; i < numssize; i++) {
 		for (int j = i+1; j < numssize; j++) {
-			if ((nums[i] + nums[j] == target) && (i != j)) {
+			if (nums[i] + nums[j] == target) {
 				indices[0] = i;
 				indices[1] = j;
 			}
