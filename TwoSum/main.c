b74a3e705c9befeb08ffae125323c5389523b1ff
One Graund <vovangaunder@gmail.com>
2025-07-27 23:03:49 +0200

    Minor TwoSum improovement

--- TwoSum/main.c
+++ TwoSum/main.c
@@ -1,27 +1,13 @@
 #include <stdlib.h>
 
-int min(const int *p1, const int *p2) {
-	if (*p1 < *p2)
-		return *p1;
-	else
-		return *p2;
-}
-
-int max(const int *p1, const int *p2) {
-	if (*p1 < *p2)
-		return *p2;
-	else
-		return *p1;
-}
-
 int *twoSum(int *nums, int numssize, int target, int *returnsize) {
 
 	int *indices = malloc(sizeof(int) * 2);
 	for (int i = 0; i < numssize; i++) {
 		for (int j = i+1; j < numssize; j++) {
 			if ((nums[i] + nums[j] == target) && (i != j)) {
-				indices[0] = min(&i, &j);
-				indices[1] = max(&i, &j);
+				indices[0] = i;
+				indices[1] = j;
 			}
 		}
 	}
